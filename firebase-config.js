9d70eefd247c195244e93793d35b8841e129191d
sreenathsjk <content2u.sj@gmail.com>
2026-03-02 11:20:32 +0530

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// firebase-config.js
+// Campus Prime Firebase Configuration
+
+const firebaseConfig = {
+apiKey: "AIzaSyDIFLO7rwIJbJnAt2EV7m57ydkPfH9FQzA",
+authDomain: "campus-prime.firebaseapp.com",
+projectId: "campus-prime",
+storageBucket: "campus-prime.firebasestorage.app",
+messagingSenderId: "934780803109",
+appId: "1:934780803109:web:678a903369166ce72f07cc"
+};
+
+export default firebaseConfig;
